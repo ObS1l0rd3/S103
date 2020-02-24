2388f55882bc88677d7f0c7605215388e109a645
King-Dinero <60258194+King-Dinero@users.noreply.github.com>
2020-02-24 17:22:47 +0100

    Add files via upload

---
+++ RRReflexPRGM/RRReflexPrgm.js
@@ -0,0 +1,10 @@
+//programme reflex clavier
+function testouille(){
+    var timer = [5000,5500,6000,6500,7000,7500,9000,9500,10000]
+    var randtmr = timer[Math.floor(Math.random()*timer.length)]
+    setTimeout(ntm, randtmr)
+}
+
+function ntm(){
+    alert("Ca marche wllh.")
+}
